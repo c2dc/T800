ddb099cf9b5283f400e5db0ecca348d2a962997d
César Guibo <cesarguibo0@gmail.com>
2022-08-17 21:00:51 -0300

    Added the influence of factors analysis for the non energy related metrics

--- collected_data/exp/format_data.py
+++ collected_data/exp/format_data.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
         with open(f'{RAW_FILES_DIR}/{file_name}', 'r') as in_file:
             header_pattern = r'[A-Za-z_\s]+[A-Za-z_,0-9\s]+\n[^a-zA-Z_]+'
             csvs = [re.sub('Tmr Svc', 'TmrSvc', csv.strip('\n ')) for csv in re.findall(header_pattern, in_file.read())]
-        for i, csv in enumerate(csvs):
+        for i, csv in enumerate(csvs[:30]):
             print(f'\twriting {file_name}_{i}')
             with open(f'{FORMATED_FILES_DIR}/{file_name[:-4]}_{i}.csv', 'w') as out_file:
                 out_file.write(csv)
